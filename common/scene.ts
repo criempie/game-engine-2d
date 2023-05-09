0a1c469408dd9759109ba5bf9b4747e46ba0ceba
criempie <crimsnemptnss@yandex.ru>
2023-05-10 01:47:57 +0500

    scene;

---
+++ common/scene.ts
@@ -0,0 +1,21 @@
+import { Entity, StaticEntity } from './entity';
+import { IRenderable, IUpdateable } from './types';
+
+class Scene implements IUpdateable, IRenderable {
+    public entities: Array<StaticEntity | Entity> = [];
+
+    constructor() {}
+
+    public update() {
+        this.entities.forEach((e) => {
+            if (e instanceof Entity) {
+                e.update();
+            }
+        });
+    }
+
+    public render(ctx: CanvasRenderingContext2D) {
+        this.entities.forEach((e) => e.render(ctx));
+    }
+
+}
