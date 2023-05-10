37a01cdcba15553805679bf1e0c98247114c284e
criempie <crimsnemptnss@yandex.ru>
2023-05-10 20:56:08 +0500

    add speed and direction to entity;

--- common/entity/entity.ts
+++ common/entity/entity.ts
@@ -3,6 +3,9 @@ import { Vector } from '../vector';
 import { StaticEntity } from './static-entity';
 
 abstract class Entity extends StaticEntity implements IUpdateable {
+    protected _speed: number = 0;
+    protected _direction: Vector = Vector.create(0, 0);
+
     constructor(position: Vector, size: Vector) {
         super(position, size);
     }
